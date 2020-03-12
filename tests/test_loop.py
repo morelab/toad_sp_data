f164d379462e9490604497c646830ab38e26b454
mikelsr <mikelsr@pm.me>
2020-03-12 16:44:24 +0100

    Add test for Loop class

---
+++ tests/test_loop.py
@@ -0,0 +1,17 @@
+import asyncio
+import pytest
+from toad_sp_data.loop import Loop
+
+_LOOP_NUMBER = 3  # loops to be created at tests
+
+
+@pytest.mark.asyncio
+async def test_loop():
+    async def asleep(x):
+        await asyncio.sleep(x)
+
+    loops = [Loop(asleep, (3,)) for _ in range(_LOOP_NUMBER + 1)]
+    for loop in loops:
+        loop.start()
+    for loop in loops:
+        await loop.stop()
