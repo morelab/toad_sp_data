03b2a468213f5203f22e4497008ba8a0df552e52
mikelsr <mikelsr@pm.me>
2020-03-12 16:01:24 +0100

    Add Loop class for future async function loops

---
+++ toad_sp_data/loop.py
@@ -0,0 +1,52 @@
+import asyncio
+from typing import Any, Callable, Tuple
+
+
+class Loop:
+    def __init__(self, async_func: Callable[..., Any], arguments: Tuple[Any, ...]):
+        """
+        A wrapper to run an asynchronous function in a loop until manually
+        cancelled.
+
+        :param async_func: asynchronous function to run
+        :param arguments: arguments to pass to said function
+        """
+
+        async def func(*args: Any):
+            """
+            Wrapper function for async_func to ensure it stops at
+            asyncio.CancelledError unless implemented otherwise inside
+            async_func.
+
+            :param args: args to pass to async_function
+            :return: None
+            """
+            try:
+                while True:
+                    await async_func(*args)
+            except asyncio.CancelledError:
+                pass
+
+        self.func = func
+        self.args = arguments
+        self.task: asyncio.Task = None
+
+    def start(self) -> None:
+        """
+        Create an asyncio.Task for self.func which will run async_func.
+
+        :return: None
+        """
+        self.task = asyncio.create_task(self.func(*self.args))
+
+    async def stop(self) -> None:
+        """
+        Cancel self.func and wait for the cancellation to finish.
+
+        :return: None
+        """
+        self.task.cancel()
+        try:
+            await self.task
+        except asyncio.CancelledError:
+            pass
